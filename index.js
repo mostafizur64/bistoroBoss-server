02ad50f58c3b15e194c8aba48eef86c5daf942ef
tarafdar16 <imrantarafdar16@gmail.com>
2024-01-28 14:56:05 +0600

    done

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
 
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewsCollection = client.db("bistroDB").collection("reviews");
+    const cartCollection = client.db("bistroDB").collection("carts");
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
@@ -35,6 +36,25 @@ async function run() {
 
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // cart collections
+
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await cartCollection.insertOne(item);
       res.send(result);
     });
 
@@ -56,3 +76,16 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Bistro Boss is sitting on port ${port}`);
 });
+
+/**
+ * ===========================================
+ * Naming Convention
+ * ===========================================
+ * user:userCollections;
+ * app.get('/users')
+ * app.get('/users/:id')
+ * app.post('users')
+ * app.patch('users/:id');
+ * app.put("user/:id");
+ * app.delete("user/:id')
+ */
