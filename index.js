6de22b5a5cb05180e7fac21a22db94b5152d790b
tarafdar16 <imrantarafdar16@gmail.com>
2024-02-09 00:28:48 +0600

    done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -30,6 +31,7 @@ async function run() {
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewsCollection = client.db("bistroDB").collection("reviews");
     const cartCollection = client.db("bistroDB").collection("carts");
+    const paymentCollection = client.db("bistroDB").collection("payments");
 
     //  jwt token
     app.post("/jwt", (req, res) => {
@@ -140,7 +142,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/menu/:id",verifyToken,verifyAdmin, async (req, res) => {
+    app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await menuCollection.deleteOne(query);
@@ -182,6 +184,92 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // create payment intent
+
+    app.post("/create-payment-intent", verifyToken, async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    // payment related api ===============
+    app.post("/payment", verifyToken, async (req, res) => {
+      const payment = req.body;
+      const insertResult = await paymentCollection.insertOne(payment);
+      const query = {
+        _id: { $in: payment.cartItems.map((id) => new ObjectId(id)) },
+      };
+      const deleteResult = await cartCollection.deleteMany(query);
+      res.send({ insertResult, deleteResult });
+    });
+
+    app.get("/admin-stats", verifyToken, verifyAdmin, async (req, res) => {
+      const users = await usersCollection.estimatedDocumentCount();
+      const products = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      // bet way to get sum of the a field is to use group and sum operator
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce((sum, payment) => sum + payment.price, 0);
+      res.send({
+        revenue,
+        users,
+        products,
+        orders,
+      });
+    });
+
+    /**
+     * ---------------------
+     * bangla system (second best solution)
+     *
+     * 1.load all payments
+     * 2.for each payment,get the menuItems array
+     * 3.for each item in the menuItems array get the menuitem from the menu collection
+     * 4.put them in an array:allOrderItems
+     * 5.separate allOrderedItems by category using filter
+     * 6.now get the quantity by using length: pizza.length
+     * 7.for each category use recude to get the total amount spent on this category
+     *
+     */
+    app.get("/order-stats", verifyToken, verifyAdmin, async (req, res) => {
+      const pipeline = [
+        {
+          $lookup: {
+            from: "menu",
+            localField: "menuItems",
+            foreignField: "_id",
+            as: "menuItemsData",
+          },
+        },
+        { $unwind: "$menuItemsData" },
+        {
+          $group: {
+            _id: "$menuItemsData.category",
+            count: { $sum: 1 },
+            totalPrice: { $sum: "$menuItemsData.price" },
+          },
+        },
+        {
+          $project: {
+            category: "$_id",
+            count: "1",
+            total: { $round: ["$total", 2] },
+            _id: 0,
+          },
+        },
+      ];
+      const result = await paymentCollection.aggregate(pipeline).toArray();
       res.send(result);
     });
 
