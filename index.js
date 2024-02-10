573eefe02784885f8d561a81a9910b4896f96bfd
tarafdar16 <imrantarafdar16@gmail.com>
2024-02-11 00:20:42 +0600

    done

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await menuCollection.deleteOne(query);
-      res.send(result);
+      res.send(result);  
     });
 
     // review related api
